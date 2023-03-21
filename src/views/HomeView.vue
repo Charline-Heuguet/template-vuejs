e037aa43edac62686364219ffd10233937830bea
Charline-Heuguet <charline.heuguet@gmail.com>
2023-03-21 17:05:04 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+//import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>titre de la page</h1>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
